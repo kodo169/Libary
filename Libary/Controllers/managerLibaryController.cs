a45e6bffacda9c7baac2f860bf9a647ed84b9402
Vũ Huỳnh <vuhuynh1692003@gmail.com>
2024-04-30 16:01:07 +0700

    giao diện Libary Manager

---
+++ Libary/Controllers/managerLibaryController.cs
@@ -0,0 +1,18 @@
+﻿using Libary.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libary.Controllers
+{
+    public class managerLibaryController : Controller
+    {
+        private readonly LibaryContext _data;
+        public managerLibaryController(LibaryContext data)
+        {
+            _data = data;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
