f57b065f51a8b75b6e6e252825eb66a9c61567e5
TranVuThanhLam <thanhlam20062003@gmail.com>
2024-05-21 08:21:04 +0700

    viet xong ham tang so luong vat pham trong gio hang

--- Libary/wwwroot/js/site.js
+++ Libary/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Tăng số lượng sách trong giỏ hàng
+$(".AddToCart_Btn").on("click", function(){
+    let x = 0;
+    x = 0 + Number($("#NumberItem_Cart").text());
+    $("#NumberItem_Cart").text
+    (
+        (
+            x + 1
+        )
+    );
+});
