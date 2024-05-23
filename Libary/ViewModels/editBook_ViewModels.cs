f4dbea32a2dc88b98354c866759b11763cc6db28
Nguyen Huu Hoang <h02092003@gmail.com>
2024-05-24 00:00:21 +0700

    add edit book

---
+++ Libary/ViewModels/editBook_ViewModels.cs
@@ -0,0 +1,33 @@
+﻿using Libary.Data;
+
+namespace Libary.ViewModels
+{
+    public class editBook_ViewModels
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public DateOnly? PublicationYear { get; set; }
+
+        public int NumberBook { get; set; }
+
+        public string PictureBook { get; set; } = null!;
+
+        public int AuthorId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int? Price { get; set; }
+
+        public DateOnly? DateCreate { get; set; }
+
+        public virtual Author Author { get; set; } = null!;
+
+        public virtual ICollection<BillInfo> BillInfos { get; set; } = new List<BillInfo>();
+
+        public virtual Category Category { get; set; } = null!;
+    }
+}
