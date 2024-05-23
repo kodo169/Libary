1e27e26ce369e9662f590e4b373cd6a47e977aeb
Vũ Huỳnh <vuhuynh1692003@gmail.com>
2024-05-23 13:09:34 +0700

    rac

---
+++ Libary/Controllers/addCartController.cs
@@ -0,0 +1,18 @@
+﻿using Libary.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libary.Controllers
+{
+    public class addCartController : Controller
+    {
+        private readonly LibaryContext _data;
+        public addCartController(LibaryContext data)
+        {
+            _data = data;
+        }
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
